3116c1183d9bef8d0aba84552a6c572b73f4f331
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-12 15:58:06 +0300

    Rename alarm in reminder

--- app/src/main/java/com/example/workouthome/fragments/ReminderFragment.kt
+++ app/src/main/java/com/example/workouthome/fragments/ReminderFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.workouthome.R
 
-class AlarmFragment : Fragment() {
+class ReminderFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false)
+        return inflater.inflate(R.layout.fragment_reminder, container, false)
     }
 }
