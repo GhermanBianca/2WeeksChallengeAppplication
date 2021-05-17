d2a9e438413393bc71016f37f38b53ecc84a822c
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-17 10:18:36 +0300

    Add drink activity functionality

--- app/src/main/java/com/example/workouthome/activities/DrinkWaterActivity.kt
+++ app/src/main/java/com/example/workouthome/activities/DrinkWaterActivity.kt
@@ -1,13 +1,60 @@
 package com.example.workouthome.activities
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.example.workouthome.R
+import kotlinx.android.synthetic.main.activity_drink_water.*
 
 class DrinkWaterActivity : AppCompatActivity() {
+
+    private var isChecked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drink_water)
+
+        // for show app full screen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        setupActionBar()
+        var checkAnim = findViewById<LottieAnimationView>(R.id.checkbox_water)
+        checkAnim.setMinFrame(20)
+        checkAnim.setMaxFrame(50)
+
+        checkAnim.setOnClickListener {
+            if(isChecked) {
+                checkAnim.speed = 1f
+                checkAnim.playAnimation()
+                isChecked= true
+            } else {
+                checkAnim.speed = -1f
+                checkAnim.playAnimation()
+                isChecked= false
+            }
+        }
+    }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_drink_water)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_back)
+        }
+        toolbar_drink_water.setNavigationOnClickListener { onBackPressed() }
     }
 }
