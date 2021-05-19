82f3c852ee8afabf230135d5ffae19c4697af3f4
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-19 17:06:15 +0300

    Add annotation for userEmail and wasActivated

--- app/src/main/java/com/example/workouthome/roomdb/NotificationEntity.kt
+++ app/src/main/java/com/example/workouthome/roomdb/NotificationEntity.kt
@@ -1,10 +1,15 @@
 package com.example.workouthome.roomdb
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 class NotificationEntity {
 
+    @PrimaryKey
     var userEmail : String = ""
+
+    @ColumnInfo (name = "NOTIFICATION_STATUS")
     var wasActivated : Boolean = false
 }
