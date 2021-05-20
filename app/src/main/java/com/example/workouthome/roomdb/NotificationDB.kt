7b2a7901ca4efe33b06b8a2909b3a5f285e12f65
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-20 08:35:35 +0300

    Add class annotation

--- app/src/main/java/com/example/workouthome/roomdb/NotificationDB.kt
+++ app/src/main/java/com/example/workouthome/roomdb/NotificationDB.kt
@@ -1,7 +1,9 @@
 package com.example.workouthome.roomdb
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database (entities = [(NotificationEntity::class)], version = 1)
 abstract class NotificationDB : RoomDatabase(){
 
     abstract fun notificationDAO() : NotificationDAO
