c456222818a491a9f669dbca6947d10adce511d7
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-20 08:31:35 +0300

    Add notificationDAO function

--- app/src/main/java/com/example/workouthome/roomdb/NotificationDB.kt
+++ app/src/main/java/com/example/workouthome/roomdb/NotificationDB.kt
@@ -3,4 +3,6 @@ package com.example.workouthome.roomdb
 import androidx.room.RoomDatabase
 
 abstract class NotificationDB : RoomDatabase(){
+
+    abstract fun notificationDAO() : NotificationDAO
 }
