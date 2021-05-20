0a55fe7a345027f39f32e716042dfa437657e928
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-20 09:54:51 +0300

    Add onConflict for eliminate error

--- app/src/main/java/com/example/workouthome/roomdb/NotificationDAO.kt
+++ app/src/main/java/com/example/workouthome/roomdb/NotificationDAO.kt
@@ -2,14 +2,15 @@ package com.example.workouthome.roomdb
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface NotificationDAO {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun saveNotification( notification : NotificationEntity)
 
-    @Query( "select * from NotificationEntity")
-    fun readNotification() : List<NotificationEntity>
+   /* @Query( "select * from NotificationEntity")
+    fun readNotification() : List<NotificationEntity>*/
 }
