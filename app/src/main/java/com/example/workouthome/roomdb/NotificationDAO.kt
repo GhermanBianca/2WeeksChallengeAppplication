5144ce12d7964857dded4428ae704515941af1da
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-20 08:26:18 +0300

    Add saveNotification function with @Insert annotation

--- app/src/main/java/com/example/workouthome/roomdb/NotificationDAO.kt
+++ app/src/main/java/com/example/workouthome/roomdb/NotificationDAO.kt
@@ -1,7 +1,11 @@
 package com.example.workouthome.roomdb
 
 import androidx.room.Dao
+import androidx.room.Insert
 
 @Dao
 interface NotificationDAO {
+
+    @Insert
+    fun saveNotification( notification : NotificationEntity)
 }
