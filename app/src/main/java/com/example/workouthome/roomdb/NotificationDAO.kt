091289ad39e4377e65a3228562c12eb66ce3fe87
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-05-20 09:18:10 +0300

    Create Function for read notification

--- app/src/main/java/com/example/workouthome/roomdb/NotificationDAO.kt
+++ app/src/main/java/com/example/workouthome/roomdb/NotificationDAO.kt
@@ -2,10 +2,14 @@ package com.example.workouthome.roomdb
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface NotificationDAO {
 
     @Insert
     fun saveNotification( notification : NotificationEntity)
+
+    @Query( "select * from NotificationEntity")
+    fun readNotification() : List<NotificationEntity>
 }
