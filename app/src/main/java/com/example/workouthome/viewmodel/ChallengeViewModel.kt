dade6c6e6dd8e0d7894746870d0abbe16881ad0d
Gherman Bianca <bianca.gherman@asperitas-solutions.com>
2021-06-02 08:38:05 +0300

    Add description list

--- app/src/main/java/com/example/workouthome/viewmodel/ChallengeViewModel.kt
+++ app/src/main/java/com/example/workouthome/viewmodel/ChallengeViewModel.kt
@@ -19,6 +19,19 @@ class ChallengeViewModel {
                 videoPath =
                     "android.resource://" + "com.example.workouthome" + "/" + R.raw.test_android
             }
+            4 -> {
+                videoPath =
+                    "android.resource://" + "com.example.workouthome" + "/" + R.raw.test_android
+            }
+            5 -> {
+                videoPath =
+                    "android.resource://" + "com.example.workouthome" + "/" + R.raw.test_android
+            }
+
+            6 -> {
+                videoPath =
+                    "android.resource://" + "com.example.workouthome" + "/" + R.raw.test_android
+            }
         }
         return videoPath
     }
@@ -33,7 +46,7 @@ class ChallengeViewModel {
                         "La semiflotare coatele merg în diagonală, fesele sunt bine încordate, stând paralele cu solul. Dacă poți, fă flotările din picioare. \n" +
                         "La genoflexiuni, trebuie să ții picioarele la nivelul umerilor și vârfurile puțin deschise spre exterior. În momentul în care cobori, fesele sunt cele care coboară primele, nu lăsa presiunea pe genunchi. \n" +
                         "Inspiră pe nas, expiră pe gură și fă pauze de apă(20-30 secunde). \n" +
-                        "Varianta mai ușoară la exercițiul cu rostogolirea(min 19) este să ții picioarele îndoite. Poziția spatelui să fie mereu cu umerii trași în față și abdomenul bine încordat în interior. \n" +
+                        "Varianta mai ușoară la exercițiul cu rostogolirea este să ții picioarele îndoite. Poziția spatelui să fie mereu cu umerii trași în față și abdomenul bine încordat în interior. \n" +
                         "La fandări, pasul cât mai mare iar genunchiul nu depășește vârful degetelor. \n" +
                         "Pentru exercițiile la sol vei avea nevoie de o saltea sau ceva pe care te simți comod."
             }
@@ -44,8 +57,34 @@ class ChallengeViewModel {
             }
             3 -> {
                 description = "Încordează bine abdomenul în timpul exercițiilor. \n" +
-                        "Strânge bine pumnii și lovește cu fortă. \n" +
-                        "La abdomene ai grijă la poziția spatelui, abdomenul trebuie să fie băgat în interior, lombarii lipiți de podea, gâtul în sus, umerii ridicați și pieptul în față."
+                        "Strânge bine pumnii și lovește cu forță. \n" +
+                        "La abdomene ai grijă la poziția spatelui, abdomenul trebuie să fie băgat în interior, lombarii lipiți de podea, gâtul în sus, umerii ridicați și pieptul în față. \n" +
+                        "Nu uita să faci pauze pentru a bea apă! \n" +
+                        "Ține mereu abdomenul încordat!"
+            }
+
+            4 -> {
+                description = "Dacă ești avansat, te rog să faci exercițiile cu greutăți. \n " +
+                        "La genoflexiuni fesele merg mereu în spate, vârfurile picioarelor sunt perfect deschise, genunchii în exterior iar coloana dreaptă. \n" +
+                        "La flotări, varianta ușoară este din genunchi. \n" +
+                        "Nu uita să faci pauze pentru a bea apă! \n" +
+                        "Ține mereu abdomenul încordat!"
+            }
+            5 -> {
+                description = "La exercițiul din minutul 03.44 ține vârfurile puțin în diagonală, spatele drept, fesele mult în spate și fă pași laterali cu lovitură. \n " +
+                        "La exercițiile de fight ține abdomenul încordat și pumnul strâns.\n" +
+                        "La fandări, unghiul trebuie să fie de 90 de grade, spatele să nu fie cocoșat, ci în forma lui naturală perpendiculară, iar la ridicare încordează fesele. \n" +
+                        "Dacă nu ai benzi de fese poți face fără, dar îți recomand să îți cumperi pentru rezultate garantate. \n" +
+                        "Nu uita să faci pauze pentru a bea apă! \n" +
+                        "Ține mereu abdomenul încordat!"
+            }
+            6 -> {
+                description = "Termenul ‘Yoga’ este derivat din rădăcina Sanskrită ‘yuj’, care înseamă uniune - uniunea dintre corp, minte şi spirit sau conştiinţă. Este un mod de a uni diferite aspecte a ceea ce suntem noi. \n" +
+                        "Yoga reduce stresul și anxietatea, implicit efectele fizice ale stresului asupra organismului. \n" +
+                        "Yoga îmbunătățește flexibilitatea și echilibrul, dar crește și puterea mușchilor. \n" +
+                        "Yoga poate reduce inflamațiile din corp și poate îmbunătăți sănătatea inimii. \n"
+
+
             }
         }
         return description
